a592706ab0de2671a329193278843ec19267accc
Jason Wu <zwu047@hotmail.com>
2022-02-19 22:04:59 +1300

    refactoring the main class

---
+++ GroceryMarket/Solution.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace GroceryMarket
+{
+    public class Solution
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("start");
+        }
+    }
+}
