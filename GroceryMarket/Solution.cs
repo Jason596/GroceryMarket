5c92a99a80da050a45b6e3b98c4e4b21326ff77d
Jason Wu <zwu047@hotmail.com>
2022-02-20 15:48:46 +1300

    Updated the main solution method

--- GroceryMarket/Solution.cs
+++ GroceryMarket/Solution.cs
@@ -1,4 +1,5 @@
 using System;
+using PointOfSaleTerminal;
 
 namespace GroceryMarket
 {
@@ -6,7 +7,31 @@ namespace GroceryMarket
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("start");
+            while (true)
+            {
+                Console.WriteLine("Starting the Grocery Market Program...");
+                Console.WriteLine("Please scan the product...");
+
+                var products = Console.ReadLine();
+                if (products == null)
+                {
+                    Console.WriteLine("Fail to scan the product, please try again.");
+                    continue;
+                }
+
+                Console.WriteLine($"The product we have scanned are {products}");
+                var terminal = new PointOfSaleCalculator(products.ToUpper());
+
+                var totalPrice = terminal.CalculateTotal();
+                Console.WriteLine($"The total grocery price is {totalPrice}");
+                Console.WriteLine();
+
+                Console.WriteLine("Continue check out, type YES/NO ");
+                var stop = Console.ReadLine();
+                if (stop != null && stop.ToLower() == "yes") continue;
+
+                break;
+            }
         }
     }
 }
